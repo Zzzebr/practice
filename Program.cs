12b9be515352e81c18b1e00aac7db0066b30e6e1
Zefir786 <crazycrazybull55@gmail.com>
2025-03-25 10:09:53 +0300

    commit from another account

--- Program.cs
+++ Program.cs
@@ -27,6 +27,25 @@ namespace ConsoleApp6
             Console.WriteLine("\tm - Multiply");
             Console.WriteLine("\td - Divide");
             Console.Write("Your option? ");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
         }
     }
 }
